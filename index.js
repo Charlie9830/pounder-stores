2825f433a992045cf9897f7516494b962ed7446a
Charlie Hall <charlie.hall@ozemail.com.au>
2017-10-23 19:15:33 +1100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+export class ProjectStore {
+    constructor(projectName, uid) {
+        this.projectName = projectName;
+        this.uid = uid;
+    }
+}
+
+export class ProjectLayoutStore {
+    constructor(layouts, project, uid) {
+        this.layouts = layouts;
+        this.project = project;
+        this.uid = uid;
+    } 
+}
+
+export class TaskListSettingsStore {
+    constructor(isCompleteTasksShown, sortBy) {
+        this.isCompleteTasksShown = isCompleteTasksShown;
+        this.sortBy = sortBy;
+    }
+}
+
+export class TaskListStore {
+    constructor(taskListName, project, uid, taskListId, settings) {
+        this.taskListName = taskListName;
+        this.project = project;
+        this.uid = uid;
+        this.taskListId = taskListId;
+        this.settings = settings;
+    }
+}
+
+export class TaskStore {
+    constructor(taskName, dueDate, isComplete, project, taskList, uid, dateAdded, isNewTask) {
+        this.taskName = taskName;
+        this.dueDate = dueDate;
+        this.isComplete = isComplete;
+        this.project = project;
+        this.taskList = taskList;
+        this.uid = uid;
+        this.dateAdded = dateAdded;
+        this.isNewTask = isNewTask;
+    }
+}
