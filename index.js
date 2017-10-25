b7bd7a594175788f9fa39ee2c88bb240735e4592
Charlie Hall <charlie.hall@ozemail.com.au>
2017-10-25 21:21:39 +1100

    Added Babil Transpilation to keep MinifyJs Happy.

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-export class ProjectStore {
-    constructor(projectName, uid) {
-        this.projectName = projectName;
-        this.uid = uid;
-    }
-}
+"use strict";
 
-export class ProjectLayoutStore {
-    constructor(layouts, project, uid) {
-        this.layouts = layouts;
-        this.project = project;
-        this.uid = uid;
-    } 
-}
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 
-export class TaskListSettingsStore {
-    constructor(isCompleteTasksShown, sortBy) {
-        this.isCompleteTasksShown = isCompleteTasksShown;
-        this.sortBy = sortBy;
-    }
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-export class TaskListStore {
-    constructor(taskListName, project, uid, taskListId, settings) {
-        this.taskListName = taskListName;
-        this.project = project;
-        this.uid = uid;
-        this.taskListId = taskListId;
-        this.settings = settings;
-    }
-}
+var ProjectStore = exports.ProjectStore = function ProjectStore(projectName, uid) {
+    _classCallCheck(this, ProjectStore);
 
-export class TaskStore {
-    constructor(taskName, dueDate, isComplete, project, taskList, uid, dateAdded, isNewTask) {
-        this.taskName = taskName;
-        this.dueDate = dueDate;
-        this.isComplete = isComplete;
-        this.project = project;
-        this.taskList = taskList;
-        this.uid = uid;
-        this.dateAdded = dateAdded;
-        this.isNewTask = isNewTask;
-    }
-}
+    this.projectName = projectName;
+    this.uid = uid;
+};
+
+var ProjectLayoutStore = exports.ProjectLayoutStore = function ProjectLayoutStore(layouts, project, uid) {
+    _classCallCheck(this, ProjectLayoutStore);
+
+    this.layouts = layouts;
+    this.project = project;
+    this.uid = uid;
+};
+
+var TaskListSettingsStore = exports.TaskListSettingsStore = function TaskListSettingsStore(isCompleteTasksShown, sortBy) {
+    _classCallCheck(this, TaskListSettingsStore);
+
+    this.isCompleteTasksShown = isCompleteTasksShown;
+    this.sortBy = sortBy;
+};
+
+var TaskListStore = exports.TaskListStore = function TaskListStore(taskListName, project, uid, taskListId, settings) {
+    _classCallCheck(this, TaskListStore);
+
+    this.taskListName = taskListName;
+    this.project = project;
+    this.uid = uid;
+    this.taskListId = taskListId;
+    this.settings = settings;
+};
+
+var TaskStore = exports.TaskStore = function TaskStore(taskName, dueDate, isComplete, project, taskList, uid, dateAdded, isNewTask) {
+    _classCallCheck(this, TaskStore);
+
+    this.taskName = taskName;
+    this.dueDate = dueDate;
+    this.isComplete = isComplete;
+    this.project = project;
+    this.taskList = taskList;
+    this.uid = uid;
+    this.dateAdded = dateAdded;
+    this.isNewTask = isNewTask;
+};
