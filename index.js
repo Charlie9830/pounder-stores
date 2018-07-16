fa0e292ca49c11c312d42a71bd86974652be83f0
Charlie Hall <charlie.hall@ozemail.com.au>
2018-07-16 16:04:54 +1000

    Updated gitignore to ignore build output.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+exports.GetFreshLayout = GetFreshLayout;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -16,11 +17,34 @@ var DatabaseStore = exports.DatabaseStore = function DatabaseStore(projects, pro
     this.accountConfig = accountConfig;
 };
 
-var ProjectStore = exports.ProjectStore = function ProjectStore(projectName, uid) {
+var ProjectStore = exports.ProjectStore = function ProjectStore(projectName, uid, isRemote) {
     _classCallCheck(this, ProjectStore);
 
     this.projectName = projectName;
     this.uid = uid;
+    this.isRemote = isRemote;
+};
+
+var RemoteStore = exports.RemoteStore = function RemoteStore(projectName, uid, members, projectLayout, taskLists, tasks) {
+    _classCallCheck(this, RemoteStore);
+
+    this.projectName = projectName;
+    this.uid = uid;
+    this.members = members;
+    this.projectLayout = projectLayout;
+    this.taskLists = taskLists;
+    this.tasks = tasks;
+};
+
+var MemberStore = exports.MemberStore = function MemberStore(userId, project, displayName, email, status, role) {
+    _classCallCheck(this, MemberStore);
+
+    this.project = project;
+    this.userId = userId;
+    this.displayName = displayName;
+    this.email = email;
+    this.status = status;
+    this.role = role;
 };
 
 var ProjectLayoutStore = exports.ProjectLayoutStore = function ProjectLayoutStore(layouts, project, uid) {
@@ -38,7 +62,7 @@ var TaskListSettingsStore = exports.TaskListSettingsStore = function TaskListSet
     this.sortBy = sortBy;
 };
 
-var TaskListStore = exports.TaskListStore = function TaskListStore(taskListName, project, uid, taskListId, settings) {
+var TaskListStore = exports.TaskListStore = function TaskListStore(taskListName, project, uid, taskListId, settings, isFresh) {
     _classCallCheck(this, TaskListStore);
 
     this.taskListName = taskListName;
@@ -46,9 +70,10 @@ var TaskListStore = exports.TaskListStore = function TaskListStore(taskListName,
     this.uid = uid;
     this.taskListId = taskListId;
     this.settings = settings;
+    this.isFresh = isFresh;
 };
 
-var TaskStore = exports.TaskStore = function TaskStore(taskName, dueDate, isComplete, project, taskList, uid, dateAdded, isNewTask, isHighPriority) {
+var TaskStore = exports.TaskStore = function TaskStore(taskName, dueDate, isComplete, project, taskList, uid, dateAdded, isNewTask, isHighPriority, metadata, assignedTo) {
     _classCallCheck(this, TaskStore);
 
     this.taskName = taskName;
@@ -60,6 +85,8 @@ var TaskStore = exports.TaskStore = function TaskStore(taskName, dueDate, isComp
     this.dateAdded = dateAdded;
     this.isNewTask = isNewTask;
     this.isHighPriority = isHighPriority;
+    this.metadata = metadata;
+    this.assignedTo = assignedTo;
 };
 
 var AccountStore = exports.AccountStore = function AccountStore(uid, favourteProjectId) {
@@ -69,10 +96,72 @@ var AccountStore = exports.AccountStore = function AccountStore(uid, favourtePro
     this.favourteProjectId = favourteProjectId;
 };
 
+var DirectoryStore = exports.DirectoryStore = function DirectoryStore(email, displayName, userId) {
+    _classCallCheck(this, DirectoryStore);
+
+    this.email = email;
+    this.displayName = displayName;
+    this.userId = userId;
+};
+
+var InviteStore = exports.InviteStore = function InviteStore(projectName, targetUserId, sourceUserId, sourceEmail, sourceDisplayName, projectId, role) {
+    _classCallCheck(this, InviteStore);
+
+    this.projectName = projectName;
+    this.targetUserId = targetUserId;
+    this.sourceUserId = sourceUserId;
+    this.sourceEmail = sourceEmail;
+    this.sourceDisplayName = sourceDisplayName;
+    this.projectId = projectId;
+    this.role = role;
+};
+
+var TaskMetadataStore = exports.TaskMetadataStore = function TaskMetadataStore(createdBy, createdOn, updatedBy, updatedOn, completedBy) {
+    _classCallCheck(this, TaskMetadataStore);
+
+    this.createdBy = createdBy;
+    this.createdOn = createdOn;
+    this.updatedBy = updatedBy;
+    this.updatedOn = updatedOn;
+    this.completedBy = completedBy;
+};
+
+function GetFreshLayout(taskListWidgetId) {
+    return { i: taskListWidgetId, x: 0, y: 0, w: 6, h: 4, isFresh: true };
+}
+
 var CssConfigStore = exports.CssConfigStore = {
     '--primary-color': '',
     '--background-color': '',
     '--surface-color': '',
     '--surface-color-alternate': '',
-    '--primary-button-background-image': ''
+    '--primary-active-text-color': '',
+    '--primary-inactive-text-color': '',
+    '--primary-hover-color': '',
+    '--alternate-active-text-color': '',
+    '--alternate-inactive-text-color': '',
+    '--loading-spinner-primary-color': '',
+    '--loading-spinner-secondary-color': '',
+    '--button-background-top-color': '',
+    '--button-background-bottom-color': '',
+    '--task-checkbox-color': '',
+    '--task-text-color': '',
+    '--task-selected-color': '',
+    '--task-hover-color': '',
+    '--task-divider-color': '',
+    '--overdue-color': '',
+    '--soon-color': '',
+    '--later-color': '',
+    '--today-color': '',
+    '--important-color': '',
+    '--due-date-text-color': '',
+    '--due-date-circle-color': '',
+    '--assignee-background-color': '',
+    '--assignee-text-color': '',
+    '--task-list-header-text-color': '',
+    '--task-list-header-selected-text-color': '',
+    '--task-list-header-background-color': '',
+    '--task-list-header-background-selected-color': '',
+    '--database-synced-color': '',
+    '--database-updates-pending-color': ''
 };
--- src/index.js
+++ src/index.js
@@ -9,9 +9,32 @@ export class DatabaseStore {
 }
 
 export class ProjectStore {
-    constructor(projectName, uid) {
+    constructor(projectName, uid, isRemote) {
         this.projectName = projectName;
         this.uid = uid;
+        this.isRemote = isRemote;
+    }
+}
+
+export class RemoteStore {
+    constructor(projectName, uid, members, projectLayout, taskLists, tasks) {
+        this.projectName = projectName;
+        this.uid = uid;
+        this.members = members;
+        this.projectLayout = projectLayout;
+        this.taskLists = taskLists;
+        this.tasks = tasks;
+    }
+}
+
+export class MemberStore {
+    constructor(userId, project, displayName, email, status, role) {
+        this.project = project;
+        this.userId = userId;
+        this.displayName = displayName;
+        this.email = email;
+        this.status = status;
+        this.role = role;
     }
 }
 
@@ -31,17 +54,18 @@ export class TaskListSettingsStore {
 }
 
 export class TaskListStore {
-    constructor(taskListName, project, uid, taskListId, settings) {
+    constructor(taskListName, project, uid, taskListId, settings, isFresh) {
         this.taskListName = taskListName;
         this.project = project;
         this.uid = uid;
         this.taskListId = taskListId;
         this.settings = settings;
+        this.isFresh = isFresh;
     }
 }
 
 export class TaskStore {
-    constructor(taskName, dueDate, isComplete, project, taskList, uid, dateAdded, isNewTask, isHighPriority) {
+    constructor(taskName, dueDate, isComplete, project, taskList, uid, dateAdded, isNewTask, isHighPriority, metadata, assignedTo) {
         this.taskName = taskName;
         this.dueDate = dueDate;
         this.isComplete = isComplete;
@@ -51,6 +75,8 @@ export class TaskStore {
         this.dateAdded = dateAdded;
         this.isNewTask = isNewTask;
         this.isHighPriority = isHighPriority;
+        this.metadata = metadata;
+        this.assignedTo = assignedTo;
     }
 }
 
@@ -61,10 +87,72 @@ export class AccountStore {
     }
 }
 
+export class DirectoryStore {
+    constructor(email, displayName, userId)  {
+        this.email = email;
+        this.displayName = displayName;
+        this.userId = userId;
+    }
+}
+
+export class InviteStore {
+    constructor(projectName, targetUserId, sourceUserId, sourceEmail, sourceDisplayName, projectId, role) {
+        this.projectName = projectName;
+        this.targetUserId = targetUserId;
+        this.sourceUserId = sourceUserId;
+        this.sourceEmail = sourceEmail;
+        this.sourceDisplayName = sourceDisplayName;
+        this.projectId = projectId;
+        this.role = role;
+    }
+}
+
+export class TaskMetadataStore {
+    constructor(createdBy, createdOn, updatedBy, updatedOn, completedBy) {
+        this.createdBy = createdBy;
+        this.createdOn = createdOn;
+        this.updatedBy = updatedBy;
+        this.updatedOn = updatedOn;
+        this.completedBy = completedBy;
+    }
+}
+
+export function GetFreshLayout(taskListWidgetId) {
+    return {i: taskListWidgetId, x: 0, y: 0, w: 6, h: 4, isFresh: true }
+}
+
 export var CssConfigStore = {
     '--primary-color': '',
     '--background-color': '',
     '--surface-color': '',
     '--surface-color-alternate': '',
-    '--primary-button-background-image': ''
+    '--primary-active-text-color': '',
+    '--primary-inactive-text-color': '',
+    '--primary-hover-color': '',
+    '--alternate-active-text-color': '', 
+    '--alternate-inactive-text-color': '',
+    '--loading-spinner-primary-color': '',
+    '--loading-spinner-secondary-color': '',
+    '--button-background-top-color': '',
+    '--button-background-bottom-color': '',
+    '--task-checkbox-color': '',
+    '--task-text-color': '',
+    '--task-selected-color': '',
+    '--task-hover-color': '',
+    '--task-divider-color': '',
+    '--overdue-color': '',
+    '--soon-color': '',
+    '--later-color': '',
+    '--today-color': '',
+    '--important-color': '',
+    '--due-date-text-color': '',
+    '--due-date-circle-color': '',
+    '--assignee-background-color': '',
+    '--assignee-text-color': '',
+    '--task-list-header-text-color': '',
+    '--task-list-header-selected-text-color': '',
+    '--task-list-header-background-color': '',
+    '--task-list-header-background-selected-color': '',
+    '--database-synced-color': '',
+    '--database-updates-pending-color': '',
 }
